01dc782f04b520d982d4eed766c99366c62771b3
Kai Sugahara <git@ksugahara.com>
2023-07-18 14:55:14 +0900

    Update: faster

--- dataLoader.py
+++ dataLoader.py
@@ -1,12 +1,12 @@
 import jax
+import jax.numpy as jnp
+import math
 
 class dataLoader:
 
     def __init__(self, key, X, Y, batch_size):
         
         # 保持
-        self.X = X
-        self.Y = Y
         self.batch_size = batch_size
 
         # 確認
@@ -17,10 +17,14 @@ class dataLoader:
         self.data_size = X.shape[0]
         
         # バッチ数
-        self.batch_num = self.data_size // self.batch_size
+        self.batch_num = math.ceil(self.data_size / self.batch_size)
         
-        # インデックスを生成
+        # インデックスをシャッフル
         self.shuffled_indices = jax.random.permutation(key, self.data_size)
+
+        # データの行をシャッフル
+        self.X = X[self.shuffled_indices].copy()
+        self.Y = Y[self.shuffled_indices].copy()
 
     def __iter__(self):
         
@@ -31,17 +35,20 @@ class dataLoader:
 
     def __next__(self):
         
-        if self.batch_idx > self.batch_num:
+        if self.batch_idx == self.batch_num:
             
             # 終了
             raise StopIteration()
             
         else:
             
-            # データindexをシャッフル
-            indices = self.shuffled_indices[ self.batch_idx*self.batch_size : (self.batch_idx+1)*self.batch_size ]
+            # ミニバッチ{batch_idx}を抽出
+            start_index = self.batch_size * self.batch_idx
+            slice_size = min( self.batch_size, (self.data_size - start_index) )
+            X = jax.lax.dynamic_slice_in_dim(self.X, start_index, slice_size)
+            Y = jax.lax.dynamic_slice_in_dim(self.Y, start_index, slice_size)
             
             # バッチindexを更新
             self.batch_idx += 1
             
-            return self.X[indices], self.Y[indices]
+            return X, Y
